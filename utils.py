3fc7464923a4147a105e434d4dd1123b3b72c481
Yuanhao Zhai <yuanhaozhai@gmail.com>
2022-07-01 17:00:29 -0400

    runnable

--- utils.py
+++ utils.py
@@ -4,18 +4,23 @@ import shutil
 import torch
 from torch.nn import functional as F
 
+
 def sample_gaussian(m, v):
-	sample = torch.randn(m.shape).cuda()
-	z = m + (v**0.5)*sample
-	return z
+    sample = torch.randn(m.shape).cuda()
+    z = m + (v**0.5) * sample
+    return z
+
 
 def gaussian_parameters(h, dim=-1):
-	m, h = torch.split(h, h.size(dim) // 2, dim=dim)
-	v = F.softplus(h) + 1e-8
-	return m, v
+    m, h = torch.split(h, h.size(dim) // 2, dim=dim)
+    v = F.softplus(h) + 1e-8
+    return m, v
+
 
 def kl_normal(qm, qv, pm, pv, yh):
-	element_wise = 0.5 * (torch.log(pv) - torch.log(qv) + qv / pv + (qm - pm - yh).pow(2) / pv - 1)
-	kl = element_wise.sum(-1)
-	#print("log var1", qv)
-	return kl
+    element_wise = 0.5 * (
+        torch.log(pv) - torch.log(qv) + qv / pv + (qm - pm - yh).pow(2) / pv - 1
+    )
+    kl = element_wise.sum(-1)
+    # print("log var1", qv)
+    return kl
